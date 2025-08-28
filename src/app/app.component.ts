16abec844f588a87520b7c7a193867011c2aa891
Jhean_Victo <jheanbarbosa1233@gmail.com>
2025-08-28 15:12:31 -0300

    feat: Cria a segunda interface do site KitchenAI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { RecipeGeneratorComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RecipeGeneratorComponent],
+  template: `
+  <main>
+    <app-recipe-generator></app-recipe-generator>
+  </main>
+`,
+styles: '',
 })
 export class AppComponent {
   title = 'kitchen-ai';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { RecipeGeneratorComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: HomeComponent
+        component: RecipeGeneratorComponent
     }
 ];
