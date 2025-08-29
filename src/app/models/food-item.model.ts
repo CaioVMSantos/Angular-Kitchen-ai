9bfa98206c55faae24039b18e667cb4c1834e15b
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-08-29 14:25:05 -0300

    feat: Adiciona o enum FoodCategory ao model.

--- src/app/models/food-item.model.ts
+++ src/app/models/food-item.model.ts
@@ -1,8 +1,10 @@
 // src/app/models/food-item.model.ts (exemplo de caminho)
 
+import { FoodCategory } from "./food-category-enum";
+
 export interface FoodItem {
     name: string;
-    category: string;
+    category: FoodCategory;
     quantity: number;
     validity: Date; // Usar o tipo Date é uma boa prática
   }
