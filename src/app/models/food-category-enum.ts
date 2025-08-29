247b78c479164265bdb33ec6013b1cd1d0ca8c2f
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-08-29 14:24:30 -0300

    feat: Adiciona o enum FoodCategory.

---
+++ src/app/models/food-category-enum.ts
@@ -0,0 +1,13 @@
+export enum FoodCategory{
+    FRUTA = 'FRUTAS',
+    LEGUME = 'LEGUME',
+    CARNE = 'CARNE',
+    PEIXE = 'PEIXE',
+    LATICINIO = 'LATICINIO',
+    GRAOS = 'GRAOS',
+    DOCE = 'DOCE',
+    SALGADO = 'SALGADO',
+    BEBIDA = 'BEBIDA',
+    TEMPEROS = 'TEMPEROS',
+    FAST_FOOD = 'FAST_FOOD'
+}
